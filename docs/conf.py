ae0f174362f5d0b6b969c4d6b053b860f9a85574
malaei <mojtaba.alaei@gmail.com>
2024-10-21 16:32:22 +0300

    updata docs

---
+++ docs/conf.py
@@ -0,0 +1,16 @@
+# Configuration file for the Sphinx documentation builder.
+
+# Project information
+project = 'Superhex'
+author = 'Mojtaba Alaei'
+release = '0.1'
+
+# General configuration
+extensions = []
+templates_path = ['_templates']
+exclude_patterns = []
+
+# Options for HTML output
+html_theme = 'alabaster'
+html_static_path = ['_static']
+
---
+++ docs/index.rst
@@ -0,0 +1,17 @@
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+Welcome to Superhex's documentation!
+====================================
+
+This is the main entry point of the Superhex documentation.
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   introduction
+   installation
+   usage
+
